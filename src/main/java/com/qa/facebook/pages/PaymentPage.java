1add4424ec8eb9608037b8ce7aa5e060d4163cb2
unknown <murari>
2022-08-24 10:13:53 +0530

    Created payment page

---
+++ src/main/java/com/qa/facebook/pages/PaymentPage.java
@@ -0,0 +1,10 @@
+package com.qa.facebook.pages;
+
+import com.qa.facebook.base.BasePage;
+
+public class PaymentPage extends BasePage{
+
+	public void payment() {
+		System.out.println("I am in payment page");
+	}
+}
